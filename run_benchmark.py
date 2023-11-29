d65ed1c547d8251503afda34cab4d813796f3d31
sayakpaul <spsayakpaul@gmail.com>
2023-11-29 08:59:37 +0530

    initial files.

---
+++ run_benchmark.py
@@ -0,0 +1,59 @@
+import torch 
+torch.set_float32_matmul_precision("high")
+
+from diffusers import DiffusionPipeline
+import argparse
+
+from .utils import benchmark_fn, bytes_to_giga_bytes, generate_csv_dict, write_to_csv
+
+CKPT_ID = "stabilityai/stable-diffusion-xl-base-1.0"
+PROMPT = "ghibli style, a fantasy landscape with castles"
+
+
+def load_pipeline(run_compile=False, compile_mode="reduce-overhead"):
+    pipe = DiffusionPipeline.from_pretrained(CKPT_ID, torch_dtype=torch.float16, use_safetensors=True)
+    pipe = pipe.to("cuda")
+
+    if run_compile:
+        pipe.unet.to(memory_format=torch.channels_last)
+        print("Run torch compile")
+        pipe.unet = torch.compile(pipe.unet, mode=compile_mode, fullgraph=True)
+
+    pipe.set_progress_bar_config(disable=True)
+    return pipe
+
+def run_inference(pipe, args):
+    _ = pipe(
+        prompt=PROMPT,
+        num_inference_steps=args.num_inference_steps,
+        num_images_per_prompt=args.batch_size,
+    )
+
+
+def main(args) -> dict:
+    pipeline = load_pipeline(run_compile=args.run_compile)
+
+    time = benchmark_fn(run_inference, pipeline, args)  # in seconds.
+    memory = bytes_to_giga_bytes(torch.cuda.max_memory_allocated())  # in GBs.
+    
+    csv_dict = generate_csv_dict(
+        pipeline_cls=str(pipeline.__class__.__name__), ckpt=CKPT_ID, args=args, time=time, memory=memory,
+    )
+    return csv_dict
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size", type=int, default=1)
+    parser.add_argument("--num_inference_steps", type=int, default=30)
+    parser.add_argument("--run_compile", action="store_true")
+    parser.add_argument("--compile_mode", type=str, default="reduce-overhead", choices=["reduce-overhead", "max-autotune"])
+    args = parser.parse_args()
+    
+    data_dict = main(args)
+
+    name = (
+        CKPT_ID.replace("/", "_")
+        + f"-bs@{args.batch_size}-steps@{args.num_inference_steps}-compile@{args.run_compile}-mode@{args.compile_mode}.csv"
+    )
+    write_to_csv(name, data_dict)
+    
---
+++ utils.py
@@ -0,0 +1,65 @@
+import torch
+import torch.utils.benchmark as benchmark
+import gc
+from typing import Union, Dict 
+import csv
+
+BENCHMARK_FIELDS = [
+    "pipeline_cls",
+    "ckpt_id",
+    "batch_size",
+    "num_inference_steps",
+    "run_compile",
+    "compile_mode"
+    "time (secs)",
+    "memory (gbs)",
+    "actual_gpu_memory (gbs)",
+]
+TOTAL_GPU_MEMORY = torch.cuda.get_device_properties(0).total_memory / (1024**3)
+
+
+
+def flush():
+    """Wipes off memory."""
+    gc.collect()
+    torch.cuda.empty_cache()
+    torch.cuda.reset_max_memory_allocated()
+    torch.cuda.reset_peak_memory_stats()
+
+
+def bytes_to_giga_bytes(bytes):
+    return f"{(bytes / 1024 / 1024 / 1024):.3f}"
+
+
+def benchmark_fn(f, *args, **kwargs):
+    t0 = benchmark.Timer(
+        stmt="f(*args, **kwargs)",
+        globals={"args": args, "kwargs": kwargs, "f": f},
+        num_threads=torch.get_num_threads(),
+    )
+    return f"{(t0.blocked_autorange().mean):.3f}"
+
+
+def generate_csv_dict(
+    pipeline_cls: str, ckpt: str, args, time: float, memory: float
+) -> Dict[str, Union[str, bool, float]]:
+    """Packs benchmarking data into a dictionary for latter serialization."""
+    data_dict = {
+        "pipeline_cls": pipeline_cls,
+        "ckpt_id": ckpt,
+        "batch_size": args.batch_size,
+        "num_inference_steps": args.num_inference_steps,
+        "run_compile": args.run_compile,
+        "compile_mode": args.compile_mode,
+        "time (secs)":time,
+        "memory (gbs)": memory,
+        "actual_gpu_memory (gbs)": f"{(TOTAL_GPU_MEMORY):.3f}",
+    }
+    return data_dict
+
+def write_to_csv(file_name: str, data_dict: Dict[str, Union[str, bool, float]]):
+    """Serializes a dictionary into a CSV file."""
+    with open(file_name, mode="w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=BENCHMARK_FIELDS)
+        writer.writeheader()
+        writer.writerow(data_dict)
