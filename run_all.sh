07d8ab1468ab0018bca3f73ff03b7da79b4d8cfe
sayakpaul <spsayakpaul@gmail.com>
2023-12-11 15:08:03 +0530

    change run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
-# Loop over compile_mode options
-for compile_mode in reduce-overhead max-autotune; do
-    # Loop over each boolean argument with two states: set and unset
-    for enable_fused_projections in "" "--enable_fused_projections"; do
-        for upcast_vae in "" "--upcast_vae"; do
-            for compile_unet in "" "--compile_unet"; do
-                for compile_vae in "" "--compile_vae"; do
-                    for change_comp_config in "" "--change_comp_config"; do
-                        for do_quant in "" "--do_quant"; do
-                            # Construct the command
-                            command="python run.py --compile_mode $compile_mode $enable_fused_projections $upcast_vae $compile_unet $compile_vae $change_comp_config $do_quant"
-                            
-                            # Run the command
-                            echo "Running command: $command"
-                            eval $command
-                        done
-                    done
-                done
-            done
-        done
-    done
-done
+python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae && \
+python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --change_comp_config && \
+python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --change_comp_config --enable_fused_projections && \
+python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --enable_fused_projections && \
+python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --do_quant && \
+python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --do_quant --change_comp_config && \
+python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --enable_fused_projections --do_quant && \
+python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --enable_fused_projections --do_quant --change_comp_config && python prepare_plot.py --final_csv_filename collated_results_peft.csv
