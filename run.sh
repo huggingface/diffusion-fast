89a43bd0709f158c02969ec02bba4700ae813ef7
sayakpaul <spsayakpaul@gmail.com>
2023-12-16 09:18:17 +0530

    remove int4

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae
 python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --enable_fused_projections && \
 python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --do_quant "int8dynamic" && \
 python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --do_quant "int8weightonly" && \
-python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --do_quant "int4weightonly" && \
 python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --do_quant --change_comp_config && \
 python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --enable_fused_projections --do_quant "int8dynamic" && \
 python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --enable_fused_projections --do_quant "int8dynamic" --change_comp_config && \
