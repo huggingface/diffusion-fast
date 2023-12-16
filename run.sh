3ea16e3e2d6fa014eec2b287c9da6e6f6eb77ec7
sayakpaul <spsayakpaul@gmail.com>
2023-12-16 10:01:54 +0530

    fix

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae
 python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --enable_fused_projections && \
 python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --do_quant "int8dynamic" && \
 python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --do_quant "int8weightonly" && \
-python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --do_quant --change_comp_config && \
 python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --enable_fused_projections --do_quant "int8dynamic" && \
 python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --enable_fused_projections --do_quant "int8dynamic" --change_comp_config && \
 python prepare_plot.py --final_csv_filename collated_results_peft.csv
