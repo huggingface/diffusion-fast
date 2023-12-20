14e2aca11e38fdb39c57735d4090b455d2828283
sayakpaul <spsayakpaul@gmail.com>
2023-12-20 08:05:12 +0530

    boom

--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-
-python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae && \
+python run_benchmark.py --no_sdpa --no_bf16 --compile_mode=max-autotune && \
 python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --change_comp_config && \
 python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --change_comp_config --enable_fused_projections && \
-python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --enable_fused_projections && \
-python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --do_quant "int8dynamic" && \
-python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --do_quant "int8weightonly" && \
-python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --enable_fused_projections --do_quant "int8dynamic" && \
 python run_benchmark.py --compile_unet --compile_mode=max-autotune --compile_vae --enable_fused_projections --do_quant "int8dynamic" --change_comp_config && \
-python prepare_plot.py --final_csv_filename collated_results_peft.csv
+python prepare_results.py --plot_title "SDXL, Batch Size: 1, Steps: 30" --final_csv_filename "collated_results.csv"
