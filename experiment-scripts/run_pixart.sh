33998d7ddf5ff2d87bbe942ac996c07ad5211571
sayakpaul <spsayakpaul@gmail.com>
2023-12-23 18:03:23 +0530

    specify ckpt for pixart

--- experiment-scripts/run_pixart.sh
+++ experiment-scripts/run_pixart.sh
@@ -2,8 +2,8 @@
 
 cd ..
 
-python run_benchmark_pixart.py --no_sdpa --no_bf16 && \
-python run_benchmark_pixart.py --compile_transformer --compile_mode=max-autotune --compile_vae --change_comp_config && \
-python run_benchmark_pixart.py --compile_transformer --compile_mode=max-autotune --compile_vae --change_comp_config --enable_fused_projections && \
-python run_benchmark_pixart.py --compile_transformer --compile_mode=max-autotune --compile_vae --enable_fused_projections --do_quant "int8dynamic" --change_comp_config && \
+python run_benchmark_pixart.py --ckpt "PixArt-alpha/PixArt-XL-2-1024-MS" --no_sdpa --no_bf16 && \
+python run_benchmark_pixart.py --ckpt "PixArt-alpha/PixArt-XL-2-1024-MS" --compile_transformer --compile_mode=max-autotune --compile_vae --change_comp_config && \
+python run_benchmark_pixart.py --ckpt "PixArt-alpha/PixArt-XL-2-1024-MS" --compile_transformer --compile_mode=max-autotune --compile_vae --change_comp_config --enable_fused_projections && \
+python run_benchmark_pixart.py --ckpt "PixArt-alpha/PixArt-XL-2-1024-MS" --compile_transformer --compile_mode=max-autotune --compile_vae --enable_fused_projections --do_quant "int8dynamic" --change_comp_config && \
 python prepare_results.py --plot_title "PixArt-Alpha, Batch Size: 1, Steps: 30" --final_csv_filename "collated_results.csv"
